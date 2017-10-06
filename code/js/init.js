3ee7ea64ed8600d0d32c08b370ab21d3033d2ec6
Subir <32188427+subir-unified@users.noreply.github.com>
2017-10-06 19:46:36 +0530

    Completed Desktop Version

--- code/js/init.js
+++ code/js/init.js
@@ -25,4 +25,47 @@ $(function(){
 	// }, function() {
 	// 	$('.site-header').addClass('transparent-bg');
 	// });
+
+	// Procuct Section Scroll
+	$('.nav a').on('click', function() {
+
+	    var scrollAnchor = $(this).attr('data-scroll'),
+	        scrollPoint = $('div[data-anchor="' + scrollAnchor + '"]').offset().top - 110;
+
+	    $('body,html').animate({
+	        scrollTop: scrollPoint
+	    }, 500);
+
+	    return false;
+
+	})
+
+	var stickyTop = $('.section-tab').offset().top - 67;
+	$(window).scroll(function() {
+	    var windscroll = $(window).scrollTop();
+	    if (windscroll >= stickyTop) {
+	        $('body').addClass('fixed-sec-tab');
+	        $('.pr-sections .pr-block').each(function(i) {
+	            if ($(this).position().top <= windscroll - 20) {
+	                $('.nav a.active').removeClass('active');
+	                $('.nav a').eq(i).addClass('active');
+	            }
+	        });
+	    } else {
+	        $('body').removeClass('fixed-sec-tab');
+	        $('.nav a.active').removeClass('active');
+	        $('.nav a:first').addClass('active');
+	    }
+
+	}).scroll();
+
+
 });
+
+
+
+
+
+
+
+

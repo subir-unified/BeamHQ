73b189f1f5f9e08ff9a6d5bd22665001cc7eb245
Subir <32188427+subir-unified@users.noreply.github.com>
2017-10-19 19:13:37 +0530

    menu change

--- code/js/init.js
+++ code/js/init.js
@@ -1,6 +1,6 @@
 $(function(){
 
-  if ($(window).width() < 768) {
+  if ($(window).width() > 768) {
      $('ul.nav li.dropdown').hover(function() {
       $(this).addClass('active');
         $(this).find('.dropdown-menu').stop(true, true).delay(100).fadeIn(200);
